239c2fc0f786cb830ad617484cee24a507ffb0ee
DilawarBaig <dilawaralibaig@gmail.com>
2023-09-23 16:55:20 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+"use strict";
+// Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence describing what the program does.
+// my name is Dilawar Ali Baig & Date:8/August/2023
+let famous_person = "Muhammad Ali Jinnah ";
+let message = `${famous_person}once said, “ Failure is a word unknown to me.”`;
+console.log(message);
+// my name is Dilawar Ali Baig & Date:8/August/2023
+let personName = "Eric";
+console.log(`Hello ${personName}, would you like to learn some Python today?`);
